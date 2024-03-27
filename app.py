c62c9a8388257d540a17b1deb620263ebe5e07d8
Amor Chouchane <amor.chouchane@gmail.com>
2024-03-27 15:06:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import openai
 # Set your OpenAI API key here (use environment variables or Streamlit's secrets for better security)
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+page_count= None
 def remove_suffix(s):
     if s.endswith("CV"):
         return s[:-3]  # Remove last 2 characters
@@ -298,6 +299,448 @@ def title(value):
     title_case = title_response.choices[0].message.content
     
     return title_case
+def Connecticut_summarizer(value):
+    summary =""
+    prompt_name = """
+            Give the name of the legal case, no need to pull in all of the defendants, just the first one , and if it is a person just his last name. 
+            If it is a State of the USA, just mention the State name.
+            
+            Use the following abreviation table if it occurs
+            A
+            Academy Acad.
+            Administrat[ive,ion] Admin.
+            Administrat[or,rix] Adm&#39;[r,x]
+            America[n] Am.
+            and &amp;
+            Associate Assoc.
+            Association Ass&#39;n
+            Atlantic Atl.
+            Authority Auth.
+            Automo[bile, tive] Auto.
+            Avenue Ave.
+            Attorney Atty.
+            B
+            Board Bd.
+            Broadcast[ing] Broad.
+            Brotherhood Bhd.
+            Brothers Bros.
+            Building Bldg.
+            Business Bus.
+            C
+            Casualty Cas.
+            Cent[er, re] Ctr.
+            Central Cent.
+            Chemical Chem.
+            Coalition Coal.
+            College Coll.
+            Commission Comm&#39;n
+            Commissioner Comm&#39;r
+            Committee Comm.
+            Communication Commc&#39;n
+            Community Cmty.
+            Company Co.
+            Compensation Comp.
+
+            Condominium Condo.
+            Congress[ional] Cong.
+            Consolidated Consol.
+            Construction Constr.
+            Continental Cont&#39;l
+            Cooperative Coop.
+            Corporation Corp.
+            Correction[s, al] Corr.
+            D
+            Defense Def.
+            Department Dep&#39;t
+            Detention Det.
+            Development Dev.
+            Director Dir.
+            Distribut[or, ing] Distrib.
+            District Dist.
+            Division Div.
+            E
+            East[ern] E.
+            Econom[ic, ics, ical, y] Econ.
+            Education[al] Educ.
+            Electric[al, ity] Elec.
+            Electronic Elec.
+            Engineer Eng&#39;r
+            Engineering Eng&#39;g
+            Enterprise Enter.
+            Entertainment Ent.
+            Environment Env&#39;t
+            Environmental Envtl.
+            Equality Equal.
+            Equipment Equip.
+            Examiner Exam&#39;r
+            Exchange Exch.
+            Execut[or, rix] Ex&#39;[r, x]
+            Export[er, ation] Exp.
+            F
+            Federal Fed.
+            Federation Fed&#39;n
+
+            Fidelity Fid.
+            Finance[e, ial, ing] Fin.
+            Foundation Found.
+            G
+            General Gen.
+            Government Gov&#39;t
+            Guaranty Guar.
+            H
+            Hospital Hosp.
+            Housing Hous.
+            I
+            Import[er, ation] Imp.
+            Incorporated Inc.
+            Indemnity Indem.
+            Independent Indep.
+            Industr[y, ies, ial] Indus.
+            Information Info.
+            Institut[e, ion] Inst.
+            Insurance Ins.
+            International Int&#39;l
+            Investment Inv.
+            J
+            K
+            L
+            Laboratory Lab.
+            Liability Liab.
+            Limited Ltd.
+            Litigation Litig.
+            M
+            Machine[ry] Mach.
+            Maintenance Maint.
+            Management Mgmt.
+            Manufacturer Mfr.
+            Manufacturing Mfg.
+            Maritime Mar.
+            Market Mkt.
+            Marketing Mktg.
+            Mechanic[al] Mech.
+
+            Medic[al, ine] Med.
+            Memorial Mem&#39;l
+            Merchan[t, dise, dising] Merch.
+            Metropolitan Metro.
+            Municipal Mun.
+            Mutual Mut.
+            N
+            National Nat&#39;l
+            North[ern] N.
+            Northeast[ern] Ne.
+            Northwest[ern] Nw.
+            Number No.
+            O
+            Organiz[ation, ing] Org.
+            P
+            Pacific Pac.
+            Partnership P&#39;ship
+            Person[al, nel] Pers.
+            Pharmaceutic[s, al] Pharm.
+            Preserv[e, ation] Pres.
+            Probation Prob.
+            Product[ion] Prod.
+            Professional Prof&#39;l
+            Property Prop.
+            Protection Prot.
+            Public Pub.
+            Publication Publ&#39;n
+            Publishing Publ&#39;g
+            Q
+            R
+            Railroad R.R.
+            Railway Ry.
+            Refining Ref.
+            Regional Reg&#39;l
+            Rehabilitation Rehab.
+            Reproduct[ion, ive] Reprod.
+            Resource[s] Res.
+            Restaurant Rest.
+
+            Retirement Ret.
+            Road Rd.
+            S
+            Savings Sav.
+            School[s] Sch.
+            Science Sci.
+            Secretary Sec&#39;y
+            Securit[y, ies] Sec.
+            Service Serv.
+            Shareholder S&#39;holder
+            Social Soc.
+            Society Soc&#39;y
+            South[ern] S.
+            Southwest[ern] Sw.
+            Steamship[s] S.S.
+            Street St.
+            Subcommittee Subcomm.
+            Surety Sur.
+            System[s] Sys.
+            T
+            Technology Tech.
+            Telecommunication Telecomm.
+            Tele[phone, graph] Tel.
+            Temporary Temp.
+            Township Twp.
+            Transcontinental Transcon.
+            Transport[ation] Transp.
+            Trustee Tr.
+            Turnpike Tpk.
+            U
+            Uniform Unif.
+            University Univ.
+            Utility Util.
+            United States U.S.
+            V
+            Village Vill.
+            W
+            West[ern] W.
+             extract the case name from a legal text similar to the following format:
+            [PLAINTIFF NAME] v. [DEFENDANT NAMES]
+
+            just return the name as an answer nothing else
+            """
+            
+    name_response = openai.ChatCompletion.create(
+    model = "gpt-4-turbo-preview",
+    temperature = 0.2,
+    max_tokens = 600,
+    messages = [
+        {"role": "system", "content": prompt_name},
+        {"role": "user", "content": value}
+        ]
+    )
+    print (name_response.choices[0].message.content)
+    
+    name_case = name_response.choices[0].message.content
+    summary = "CASE: " +  name_case + "  \n"
+    prompt_court_option = ("""I will send you a legal decision and you will detect the court that ruled and return it according to a table, just the court name nothing else.
+            Here is the structure of table :
+            Connecticut Supreme Court
+            Connecticut Appellate Court
+            Connecticut Superior Court
+            District Courts
+            Probate Courts
+            Family Courts
+            Small Claims Courts
+            Housing Courts
+            Workers' Compensation Commission
+            Tax Court
+            Juvenile Courts
+            Traffic Courts
+            Administrative Agencies
+            """)
+    court_response = openai.ChatCompletion.create(
+    model = "gpt-4-turbo-preview",
+    temperature = 0.2,
+    max_tokens = 600,
+    messages = [
+        {"role": "system", "content": prompt_court_option},
+        {"role": "user", "content": value}
+        ]
+    )
+    
+    print (court_response.choices[0].message.content)
+    court_case = court_response.choices[0].message.content
+    
+    prompt_num = """
+            Give the number of the legal case, 
+            
+            
+            
+
+            just return the number as an answer nothing else
+            """
+            
+    num_response = openai.ChatCompletion.create(
+    model = "gpt-4-turbo-preview",
+    temperature = 0.2,
+    max_tokens = 600,
+    messages = [
+        {"role": "system", "content": prompt_num},
+        {"role": "user", "content": value}
+        ]
+    )
+    print (num_response.choices[0].message.content)
+    
+    num_case = num_response.choices[0].message.content
+    
+    prompt_judge = "you are a US lawyer, and will read a legal decision and return the name of the judge, only the name, nothing else, in the format : Lastname, Firstname (only first letter of the Firstname). If the case is PER CURIAM, just return : per curiam. If it 's a federal case and district case, replace the first name by : U.S.D.J. Else if it 's a federal case and magistrate case, replace the first name by : U.S.M.J."
+
+    judge_response = openai.ChatCompletion.create(
+    model = "gpt-4-turbo-preview",
+    temperature = 0.2,
+    max_tokens = 600,
+    messages = [
+        {"role": "system", "content": prompt_judge},
+        {"role": "user", "content": value}
+        ]
+    )
+            
+    judge_name ="" 
+            
+    if judge_response.choices[0].message.content =="per curiam" :
+        judge_name = "per curiam"
+    elif "U.S.D.J." in judge_response.choices[0].message.content:
+        name = HumanName(judge_response.choices[0].message.content)
+        judge_name = name.last + ", U.S.D.J."
+        
+    elif "U.S.M.J." in judge_response.choices[0].message.content:
+        name = HumanName(judge_response.choices[0].message.content)
+        judge_name = name.last + ", U.S.M.J."
+                
+    else:
+        name = HumanName(judge_response.choices[0].message.content)
+        judge_name = name.last + ", J."  #.capitalize()
+            
+    
+    print (judge_response.choices[0].message.content)
+    date_response = openai.ChatCompletion.create(
+                model="gpt-4-turbo-preview",
+                temperature=0.2,
+                max_tokens=16,
+                messages=[
+                    {"role": "system", "content": "When did the judgment happen, if you can't find, look for decided date, also answer with the date only, nothing else, no additional text, just the date, and abreviate the month like this Jan. Feb. March April May June July Aug. Sept. Oct. Nov. Dec."},
+                    {"role": "user", "content": value}
+                ]
+            )
+
+            # Append the court date to the summary
+    court_date = date_response.choices[0].message.content.strip()
+
+
+    prompt_taxonomy = """ I will give you a table with taxonomy , read the legal case, You can use up to three taxonomies. The last topic is usually Civil Appeals or Criminal Appeals. The format is caps and lower case. Just return up to 3 taxonomies, separated by ",", example :  Wrongful Death, Civil Rights, Civil Appeals. Here is the table of taxonomy :
+            Administrative Law
+            Admiralty
+            Antitrust
+            Banking and Finance Laws
+            Bankruptcy
+            Civil Procedure
+            Civil Rights
+            Commercial Law
+            Constitutional Law
+            Consumer Protection
+            Contracts
+            Contractual Disputes
+            Corporate Entities
+            Corporate Governance
+            Creditors' and Debtors' Rights
+            Criminal Law
+            Damages
+            Personal Injury
+            Dispute Resolution
+            Education Law
+            Elder Law
+            Employment Benefits
+            Employment Litigation
+            Employment Compliance
+            Employment Litigation
+            Entertainment and Sports Law
+            Environmental Law
+            Evidence
+            Family Law
+            Government
+            Health Care Law
+            Immigration Law
+            Insurance Law
+            Intellectual Property
+            Internet Law
+            Judges
+            Legal Ethics and Attorney Discipline
+            Legal Malpractice
+            Labor Law
+            Employment Benefits
+            Employment Compliance
+            Employment Litigation
+            Land Use and Planning
+            Landlord/Tenant
+            Mass Tort Claims
+            Motor Vehicle Torts
+            Toxic Torts
+            Business Torts
+            Damages
+            Medical Malpractice
+            Motor Vehicle Torts
+            Products Liability
+            Public Records
+            Public Utilities
+            Real Estate
+            Securities
+            Tax
+            Telecommunications
+            Transportation
+            Trusts and Estates
+            Wrongful Death
+            Civil Appeals
+            Criminal Appeals
+            """
+    taxonomy_response = openai.ChatCompletion.create(
+    model = "gpt-4-turbo-preview",
+    temperature = 0.2,
+    max_tokens = 600,
+    messages = [
+        {"role": "system", "content": prompt_taxonomy},
+        {"role": "user", "content": value}
+        ]
+    )
+    print (taxonomy_response.choices[0].message.content)
+    practice_area = taxonomy_response.choices[0].message.content
+    
+    
+    prompt_practice = """
+            Give the industry of the legal case, 
+            
+            
+            
+
+            just return the industry as an answer nothing else
+            """
+            
+    practice_response = openai.ChatCompletion.create(
+    model = "gpt-4-turbo-preview",
+    temperature = 0.2,
+    max_tokens = 600,
+    messages = [
+        {"role": "system", "content": prompt_practice},
+        {"role": "user", "content": value}
+        ]
+    )
+    print (practice_response.choices[0].message.content)
+    
+    practice_case = practice_response.choices[0].message.content
+    prompt_title = "you are a US lawyer, and will read a legal decision and return the title of the case, only the title, nothing else, the title should describe in a sentence the case without mentioning the plaintiff and the defendants."
+
+    title_response = openai.ChatCompletion.create(
+    model = "gpt-4-turbo-preview",
+    temperature = 0.2,
+    max_tokens = 600,
+    messages = [
+        {"role": "system", "content": prompt_title},
+        {"role": "user", "content": value}
+        ]
+    )
+    print (title_response.choices[0].message.content)
+    title_case = title_response.choices[0].message.content
+    summary = f"""
+CASE: {name_case} \n
+COURT: {court_case} \n
+DOC NO: {num_case} \n
+COURT OPINION BY: {judge_name} \n
+DATE: {court_date} \n
+PAGES: {page_count} \n
+
+{text_summarizer_alternate(value)}
+
+TITLE: {title_case}               \n
+LITIGATION: Yes  \n
+PRACTICE AREA: {practice_area} \n
+INDUSTRY: {practice_case} \n
+ROLE:  
+
+
+"""
+    return summary
 
 def Texas_summarizer(value):
     summary =""
@@ -458,6 +901,7 @@ def is_positive_integer(value):
     
 # Define the Streamlit app
 def main():
+    global page_count
     st.image('MESJ.jpg')
     st.title("Legal Decision Summarizer")
 
@@ -478,7 +922,7 @@ def main():
     #     st.error("Please enter a valid positive integer for the page count.")
     
     # Create a dropdown to select the US State
-    states = ["New Jersey", "Texas"]
+    states = ["New Jersey", "Texas","Connecticut"]
     state = st.selectbox("Select a US State:", states)
     
     # Only show the page count option if the selected state is not Texas
@@ -947,6 +1391,9 @@ def main():
             
             st.markdown(f"**{legal_category}**")
             st.write(summary)
+        elif state =="Connecticut":
+            st.subheader("Summary:")
+            st.write(Connecticut_summarizer(user_input))
         elif state == "Texas":
             st.subheader("Summary:")
             st.write(Texas_summarizer(user_input))
