addbddb288712d371a568356ef004dc04a8596db
Sandesh Neupane <sandeshpkr977@gmail.com>
2024-12-09 18:02:33 +0545

    server

--- app.py
+++ app.py
@@ -550,7 +550,7 @@ def scrape_from_selenium(url: str, timeout: int = 20) -> Tuple[Optional[str], Op
         #     "profile.default_content_setting_values.media_stream": 2
         # }
         # options.add_experimental_option("prefs", prefs)
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version="120.0.6099.10900").install()), options=options)
         
         # driver.set_page_load_timeout(timeout)
         driver.get(url)
